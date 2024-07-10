e40713409611e308801c711b28ee30f26eff6ab6
Min Soo Kang <minsookang33@outlook.com>
2024-07-09 23:00:31 -0500

    Modify more styles

--- src/app/_components/section.tsx
+++ src/app/_components/section.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export function Section({ title, children }: Props) {
     return (
-        <div className="mx-auto w-3/4 mb-8">
+        <div className="mx-auto mb-8 max-w-4xl">
             <Container>
                 <h2 className="mb-4 text-2xl font-bold tracking-tighter leading-tight">
                     {title}
