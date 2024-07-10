1253305298d838a35900f1001966e11083a6f9ef
Min Soo Kang <minsookang33@outlook.com>
2024-07-09 21:30:12 -0500

    Change darkmode configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,23 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon/favicon.svg" />
       </head>
-      <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
-      >
-        <section className="text-center mt-20 mb-16">
-          <h1 className="text-3xl font-bold">
-            minnysoop
-          </h1>
-          <h2 className="mt-2">
-            mkang20 <b>[at]</b> depaul <b>[dot]</b> edu
-          </h2>
-        </section>
-        <div className="flex-grow">{children}</div>
+      <body className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}>
+          <section className="text-center mt-20 mb-16">
+            <h1 className="text-3xl font-bold">
+              minnysoop
+            </h1>
+            <h2 className="mt-2">
+              mkang20 <b>[at]</b> depaul <b>[dot]</b> edu
+            </h2>
+          </section>
+          <div className="flex-grow">{children}</div>
       </body>
     </html>
   );
