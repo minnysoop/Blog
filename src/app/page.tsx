61547a797a4c0a3138c41659be25fc0c50b04f97
Min Soo Kang <minsookang33@outlook.com>
2024-07-11 22:49:08 -0500

    Add introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,11 @@ export default function Index() {
 
   return (
     <>
-    <Section title="About">
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quisquam accusantium natus sunt autem impedit asperiores voluptatem cumque. Nihil pariatur placeat sed facilis laborum doloribus quia rerum atque, possimus nostrum iure accusamus voluptates magnam. Tempora distinctio non optio dicta ipsam obcaecati a, odit maiores atque cupiditate animi consequatur nemo enim qui doloremque unde. Quod reiciendis dignissimos ratione laboriosam odio eveniet, at cupiditate obcaecati magnam quibusdam. Inventore architecto ducimus harum nostrum cumque incidunt nemo deserunt cupiditate, facere beatae non nihil! Provident facilis vel molestiae obcaecati ipsam nam, numquam, eligendi sequi dicta vero quis sapiente, vitae dolorum sed incidunt totam fugiat. Voluptates!</div>
+    <Section title="Introduction">
+      <div className="mb-4">Hello, world! This is my blog. It's a place where I can rant, admire, and think about the things happenning around me. I reflect a lot. This blog could help me collect my thoughts. I feel fortunate to be in the midst of such fascinating technological developments (i.e. embedded systems, A.I, robotics, etc.). Without it, I could not have made this blog (well, actually, I probably could, it would just take a lot more work). Sometimes, it's crazy to think that things like technorealism/technoculture, impacts of intelligent systems on jobs, toxic digital media culture, and other similar stuff were once just mere "what if" thoughts. Now, they're real and we have scientists actually talking about them.</div>
+      <div className="mb-4">
+      So, this blog is sorta dedicated to those types of topics. However, I'm trying to approach these things in a more "personal" way. I believe that trying to gather insights on something that affects us so deeply is not achievable from reading a library book. There's just simply too much fabrication. Rather you'll find these in conversations, twitter posts, and things like that (i.e. this blog). With that, most of these posts are basically short reflections on my feelings, and thoughts on how I'm navigating through this blend of science fiction and reality. Read it or not, it's the "free will" you get when you are surfing the internet. I'm writing them anyway.
+      </div>
     </Section>
     <Section title="Blog">
       {firstFivePosts.length > 0 && <PostList posts={firstFivePosts} />}
